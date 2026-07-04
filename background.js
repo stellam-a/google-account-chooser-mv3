627bb5a7304998013c776c76d921e2b4304d3453
Stellam <mrniles01@gmail.com>
2026-07-04 22:54:27 +0530

    Update background.js + mv3 support

--- background.js
+++ background.js
@@ -2,31 +2,37 @@
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
     // Get list of Google accounts
     fetch('https://accounts.google.com/ListAccounts?gpsia=1&source=ogb&mo=1&origin=https://accounts.google.com')
-        .then(response => (
-            response.text()
-        ))
-        .then(rawText => (
-            // Parse HTML to get the inner JS script text
-            new DOMParser()
-            .parseFromString(rawText, 'application/xml')
-            .querySelector('script')
-            .innerHTML
+        .then(response => response.text())
+        .then(rawText => {
+            // MV3 Fix: Instead of DOMParser, we use regex to find the contents inside the <script> tags
+            const scriptMatch = rawText.match(/<script[^>]*>([\s\S]*?)<\/script>/i);
+            if (!scriptMatch || !scriptMatch[1]) {
+                throw new Error("Could not find script block in Google response");
+            }
             
-            // Get the first tokenized string in the JS
-            .split('\'')[1]
-            
-            // Replace JS string hex escapes (best way unfortunately, no built in function for this like for decodeURI)
-            .replace(/\\x([0-9a-fA-F]{2})/g, (match, paren) => (
-                String.fromCharCode(parseInt(paren, 16))
-            ))
+            const scriptContent = scriptMatch[1];
 
-            // Replace JS string slash and newline escapes
-            .replace(/\\\//g, '\/')
-            .replace(/\\n/g, '')
-        ))
-        .then(text => (
-            JSON.parse(text)
-        ))
-        .then(sendResponse);
-    return true // Indicates async response
+            // Get the first tokenized string in the JS (Keeping original parsing logic)
+            const tokenizedString = scriptContent.split('\'')[1];
+            if (!tokenizedString) {
+                throw new Error("Could not parse tokenized string from Google script");
+            }
+
+            return tokenizedString
+                // Replace JS string hex escapes
+                .replace(/\\x([0-9a-fA-F]{2})/g, (match, paren) => (
+                    String.fromCharCode(parseInt(paren, 16))
+                ))
+                // Replace JS string slash and newline escapes
+                .replace(/\\\//g, '\/')
+                .replace(/\\n/g, '');
+        })
+        .then(text => JSON.parse(text))
+        .then(jsonData => sendResponse(jsonData))
+        .catch(error => {
+            console.error("Error fetching accounts:", error);
+            sendResponse({ error: error.message });
+        });
+
+    return true; // Indicates async response
 });
